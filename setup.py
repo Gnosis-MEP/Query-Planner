3fdd55f08774bd979e7a779d73e7de0240b21840
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2022-09-19 12:27:04 +0100

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='query_planner',
+    version='0.1',
+    description='Plans Query related data, such as query QoS criteria ranking, and (future) service function chain',
+    author='Felipe Arruda Pontes',
+    author_email='felipe.arruda.pontes@insight-centre.org',
+    packages=['query_planner'],
+    zip_safe=False
+)
